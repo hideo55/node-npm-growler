026c16aa7dec7e107863a005a0e5fb54a8c797d3
hideo55 <hide.o.j55@gmail.com>
2011-08-16 22:36:28 +0900

    modify format

--- npm-growler.js
+++ npm-growler.js
@@ -4,10 +4,10 @@ var http = require('http');
 var growl = require('growl');
 var lru = require('lru-cache');
 var options = {
-  host : 'search.npmjs.org', path : '/_view/updated?limit=20'
+  host : 'search.npmjs.org', 
+  path : '/_view/updated?limit=20'
 };
-var Seen = {
-};
+var Seen = {};
 var checkUpdate = function () {
   http.get(options, function (res) {
     var self = this; var resBody = ''; 
