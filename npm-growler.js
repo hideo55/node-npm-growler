438d4d75bda6fbfa7506e98978ca7de2bf1450a9
hideo55 <hide.o.j55@gmail.com>
2011-10-10 17:18:49 +0900

    Add Notify icon Add -i/--interval options Change update check method

--- npm-growler.js
+++ npm-growler.js
@@ -3,12 +3,26 @@ var _ = require('underscore')._;
 var http = require('http');
 var growl = require('growl');
 var lru = require('lru-cache');
+var opts = require('opts');
+
+opts.parse([{
+  short: "i",
+  long: "interval",
+  description: "Update check interval",
+  value: true
+}],[],true);
+
+var interval = opts.get("i") || ( 300 * 1000 );
+if( !interval.match(/^[0-9]+$/) ){
+  process.exit(0);
+}
+
 var options = {
   host : 'search.npmjs.org', 
   path : '/_view/updated?limit=20'
 };
 var Seen = {};
-var checkUpdate = function () {
+function checkUpdate() {
   http.get(options, function (res) {
     var self = this; var resBody = ''; 
 	res.on('data', function (data) {
@@ -21,8 +35,11 @@ var checkUpdate = function () {
         _.each(rows, function (item) {
           var key = item.id + '-' + item.key; 
           if ( Seen[key] == undefined) {
-            Seen[key] = true; growl.notify(item.id, {
-              title : 'NPM Update'
+            Seen[key] = true; 
+            var message = item.id + ' released.';
+            growl.notify(message, {
+              title : 'NPM Update',
+              image: './npm-32.png',
             }); 
           }  
         }); 
@@ -33,8 +50,9 @@ var checkUpdate = function () {
   }).on('error', function (e) {
     console.log(e.message); 
   });
+  setTimeout(checkUpdate, interval)
 };
 
 console.log('Start growl notify...');
-checkUpdate.call();
-setInterval(checkUpdate, 300000);
+console.log('interval:' + interval + 'ms');
+checkUpdate();
